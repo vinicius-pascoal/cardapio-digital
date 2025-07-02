7cb0df50a980ae7440e2391e0718ad4b6bc43db6
vinicius-pascoal <112434231+vinicius-pascoal@users.noreply.github.com>
2025-07-02 13:11:04 +0000

    component categoria

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,14 @@
 import Image from "next/image";
 
 import logo from "../img/logo.svg";
+import CategoriaComponet from "../components/CategoriaComponet";
 
 import GraniteBackground from "../components/GraniteBackground";
 // cor (#1e2939) para destaques
+
 export default function Home() {
+  let ativo = false;
+
   return (
     <GraniteBackground>
       <main className="flex flex-col items-center h-screen text-gray-800">
@@ -14,8 +18,11 @@ export default function Home() {
           className="w-32 h-32 mb-6 mt-10"
           priority
         />
-        <h1 className="text-2xl font-bold mb-4 ">Bem-vindo ao Cardápio Digital</h1>
+        <h1 className="text-2xl font-bold mb-4 ">
+          Bem-vindo ao Cardápio Digital
+        </h1>
         <div className="w-4/5 h-1 bg-gray-800 mb-8"></div>
+          <CategoriaComponet />
       </main>
     </GraniteBackground>
   );
---
+++ src/components/CategoriaComponet.tsx
@@ -0,0 +1,46 @@
+"use client";
+
+import { useState } from "react";
+import Image from "next/image";
+import setaDireita from "../img/setDir.svg";
+import setaBaixo from "../img/setBaix.svg";
+
+export default function CategoriaComponet() {
+  const [ativo, setAtivo] = useState(false);
+
+  const toggleAtivo = () => {
+    setAtivo((prev) => !prev);
+  };
+
+  return (
+    <div className="cursor-pointer w-10/12">
+      <div
+        className="flex justify-between items-center mb-4 mr-32"
+        onClick={toggleAtivo}
+      >
+        <Image
+          src={ativo ? setaBaixo : setaDireita}
+          alt="Seta de categoria"
+          className="w-6 h-6"
+        />
+        <h2 className="text-xl font-bold ml-2">Entradas</h2>
+      </div>
+      {ativo && (
+        <ul className="list-none p-0">
+          <li className="flex justify-between items-center mb-4">
+            <span>Salada Caesar</span>
+            <span className="text-gray-600">R$ 25,00</span>
+          </li>
+          <li className="flex justify-between items-center mb-4">
+            <span>Bruschetta de Tomate</span>
+            <span className="text-gray-600">R$ 20,00</span>
+          </li>
+          <li className="flex justify-between items-center mb-4">
+            <span>Carpaccio de Carne</span>
+            <span className="text-gray-600">R$ 30,00</span>
+          </li>
+        </ul>
+      )}
+    </div>
+  );
+}
